da7ae9789a88579ab05701872038034ebfbccc03
Hamid Ali <alihamidali2@gmail.com>
2025-07-28 20:04:54 +0500

    Leads detail page

--- typescript/src/types.ts
+++ typescript/src/types.ts
@@ -433,3 +433,31 @@ export interface IProjectName {
     id: number
     name: string
 }
+
+export interface ILeadDetail {
+    id: number;
+    type: string;
+    created_at: string;
+    project?: {
+        id: number;
+        name: string;
+        description?: string;
+        status?: string;
+        messages?: any[];
+    };
+    client?: {
+        id: number;
+        first_name: string;
+        last_name: string;
+        email: string;
+        url?: string;
+        photo?: string;
+        shopify_plan?: string;
+    };
+    expert?: {
+        id: number;
+        first_name: string;
+        last_name: string;
+        email: string;
+    };
+}
